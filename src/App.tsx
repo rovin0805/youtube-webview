b1df6515a48c0beb9e9169d0bf7fc347b9d3fa74
Choi Eun Jeong <qhdrn67@naver.com>
2024-08-24 16:24:36 +0900

    project set up

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#242424'}}></SafeAreaView>
+  );
+};
+
+export default App;
